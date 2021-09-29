676dbf386faa787ca4840172e3f50505d06c7adb
TiXoN156 <tihon.bobrov@gmail.com>
2021-09-29 21:54:40 +0300

    First commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String email = "tihon.bobrov6876@gmail.com";
+        emailValidation(email);
+    }
+
+    private static void emailValidation(String email) {
+        String result = null;
+        Pattern pattern = Pattern.compile("^([\\w-\\.]+)@([\\w&&[^_]]+).[a-z]{2,}$",
+                Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.find()) {
+            result = matcher.group();
+        }
+        if (result != null) {
+            System.out.println(String.format("Адрес эл.почты %s является валидным.", email));
+        } else {
+            System.out.println(String.format("Адрес эл.почты %s не является валидным.", email));
+        }
+    }
+}
